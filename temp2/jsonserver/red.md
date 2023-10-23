4cdd3aaf1cdd9969145d9696559110dbedcbbce7
matthew <18217739656@163.com>
2023-10-23 14:47:37 +0800

    testd

--- temp2/jsonserver/red.md
+++
@@ -1,7 +0,0 @@
-
-json-server --watch ~/Documents/hk_work/solid/my-app/jsonserver/db.json --port 3006
-
-
-todo： 尝试生产的模式和其他服务
-
-
--- temp2/src/layout/partials/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-    return (
-        <div class="container mx-auto bg-white">
-            {/* wangzhan beian Footer  */}
-        </div>
-    )
-}
